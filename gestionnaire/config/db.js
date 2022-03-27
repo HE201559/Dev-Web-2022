6ae4dd54998bd4818f9274c57126c694b4b60d25
JarodEphec <j.boulangerflemal@students.ephec.be>
2022-03-27 18:09:59 +0200

    Ajout des information sur le serveur de la db sur db.js

--- gestionnaire/config/db.js
+++ gestionnaire/config/db.js
@@ -1,1 +1,9 @@
 "config db"
+var db = {
+    host     : 'vps-88d11377.vps.ovh.net',
+    database : 'gestionnaire_collection',
+    user     : 'remote',
+    password : 'Welcom@2803',
+    port     : '3306'
+};
+module.exports = {db} ;
