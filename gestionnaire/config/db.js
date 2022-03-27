ee3fc0491fac040e9d2dfc636eebc468fdc905b8
JarodEphec <92374656+JarodEphec@users.noreply.github.com>
2022-03-27 18:26:48 +0200

    Update db.js

--- gestionnaire/config/db.js
+++ gestionnaire/config/db.js
@@ -3,7 +3,7 @@ var db = {
     host     : 'vps-88d11377.vps.ovh.net',
     database : 'gestionnaire_collection',
     user     : 'remote',
-    password : 'Welcom@2803',
+    password : '*******',
     port     : '3306'
 };
 module.exports = {db} ;
